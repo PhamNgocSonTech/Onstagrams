b299f32dd02f59cbffcb2b6edfa2b3f23f6e58c1
DESKTOP-HU2AH4S\hc190 <hc19082001@gmail.com>
2022-10-24 22:28:15 +0700

    Create Folder, Routes for App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { pubRoutes } from "./Routes/public_route";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {pubRoutes.map((item, index) => {
+          const Layout =
+            item.layout === null
+              ? React.Fragment
+              : item.layout === undefined
+              ? "DefaultLayout"
+              : item.layout;
+          return (
+            <Route
+              key={index}
+              path={item.path}
+              element={
+                <Layout>
+                  <item.element />
+                </Layout>
+              }
+            ></Route>
+          );
+        })}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
