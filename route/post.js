349bafc73351cbbf8321472fb85cfb6c198dfdc7
PhamNgocSon <phamngocson7a1@gmail.com>
2022-10-24 19:16:13 +0700

    fixed timelien post

--- route/post.js
+++ route/post.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
+const User = require('../models/User')
+
 
 // router.get("/", async(req, res) =>{
 //     res.send("Post")
