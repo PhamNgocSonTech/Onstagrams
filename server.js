45d0aac334a66f09eb474b0434c7d855b0a550f4
PhamNgocSonTech <phamngocson7a1@gmail.com>
2022-11-17 02:00:26 +0700

    init new branch

--- server.js
+++ server.js
@@ -58,14 +58,14 @@ app.use("/api/notify", notifyRoute);
 app.use("/api/chat", chatRoute);
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 //   // Set static folder
-//   app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
 
 //   app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 //   });
-// }
+}
 
 app.listen(port, () => console.log("Server is running on port is:", port));
 
