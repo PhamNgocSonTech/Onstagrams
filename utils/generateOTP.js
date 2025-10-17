0a7a61245c219158ffc26f395fa1081e636200af
PhamNgocSonTech <phamngocson7a1@gmail.com>
2025-10-17 15:59:52 +0700

    feat: implement OTP generation and email verification functionality

--- utils/generateOTP.js
+++ utils/generateOTP.js
@@ -1,9 +1,3 @@
 exports.generateOTP = () => {
-  let otp = "";
-  let n = 6;
-  for (let i = 0; i <= n; i++) {
-    let randomCode = Math.floor(Math.random() * 90000) + 10000;
-    otp = +randomCode;
-  }
-  return otp;
+  return Math.floor(Math.random() * 900000 + 100000).toString();
 };
